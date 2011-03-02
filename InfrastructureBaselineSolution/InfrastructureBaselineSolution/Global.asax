954dec546ca118caf1b39863c6fbf63480e56b39
Adron Hall <adronhall@gmail.com>
2011-03-01 16:33:13 -0800

    Added the working solution project for the template.  Renamed the icons directory for all the basic infrastructure icons.  Deployed latest template.

---
+++ InfrastructureBaselineSolution/InfrastructureBaselineSolution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InfrastructureBaselineSolution.MvcApplication" Language="C#" %>
---
+++ InfrastructureBaselineSolution/InfrastructureBaselineSolution/ViewModels/Scrunchie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InfrastructureBaselineSolution.ViewModels
+{
+    public class Scrunchie
+    {
+        public string Name { get; set; }
+        public DateTime Stamp { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public int Sold { get; set; }
+    }
+}
