6337f7e17d703638ef7e7bf8a86411f7942ae4d0
Vertex Shader <bizadron@gmail.com>
2011-03-17 00:45:57 -0700

    Added the initial zen garden controller and respective views.

---
+++ Multi-Project Baseline Solution/Web/Controllers/ZenGardenController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ZenGardenController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CoLtd()
+        {
+            return View();
+        }
+
+        public ActionResult SilentStrength()
+        {
+            return View();
+        }
+    }
+}
