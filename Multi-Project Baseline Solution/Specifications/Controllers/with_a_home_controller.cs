718df1a40f4c93f189b69d4e0c78b04b91b69cb8
Adron Hall <adronhall@gmail.com>
2011-03-20 17:05:08 -0700

    Added the Machine.Specifications, NSubstitute, and related packages.

--- Multi-Project Baseline Solution/Specifications/Controllers/with_a_home_controller.cs
+++ Multi-Project Baseline Solution/Specifications/Controllers/with_a_home_controller.cs
@@ -1,7 +1,21 @@
 ﻿namespace Specifications.Controllers
 {
-    public class with_a_home_controller
-    {
+        public abstract class with_a_home_controller
+        {
+            Establish context = () =>
+                controller = new HomeController();
 
-    }
+            protected static HomeController controller;
+        }
+
+        public class when_calling_home_view_actions : with_a_home_controller
+        {
+            static ActionResult result;
+
+            Because of = () =>
+                result = controller.Index();
+
+            It should_return_index = () =>
+                result.ShouldNotBeNull();
+        }
 }
