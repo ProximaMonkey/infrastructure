1d8226caf17a09910a0ae2f6ac44239c9566c88c
Adron Hall <adronhall@gmail.com>
2011-03-23 07:31:08 -0700

    Some cleanup after the long non-merged commit from one machine to another.

--- Multi-Project Baseline Solution/Data/Repository.cs
+++ Multi-Project Baseline Solution/Data/Repository.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Data
 {
-    public class Class1
+    public class Repository
     {
     }
 }
--- Multi-Project Baseline Solution/Specifications/Controllers/with_a_home_controller.cs
+++ Multi-Project Baseline Solution/Specifications/Controllers/with_a_home_controller.cs
@@ -4,22 +4,44 @@ using Web.Controllers;
 
 namespace Specifications.Controllers
 {
-        public abstract class with_a_home_controller
-        {
-            Establish context = () =>
-                controller = new HomeController();
+    public abstract class with_a_home_controller
+    {
+        Establish context = () =>
+            controller = new HomeController();
 
-            protected static HomeController controller;
-        }
+        protected static HomeController controller;
+    }
 
-        public class when_calling_home_view_actions : with_a_home_controller
-        {
-            static ActionResult result;
+    public class when_calling_index : with_a_home_controller
+    {
+        static ActionResult result;
 
-            Because of = () =>
-                result = controller.Index();
+        Because of = () =>
+            result = controller.Index();
 
-            It should_return_index = () =>
-                result.ShouldNotBeNull();
-        }
+        It should_return_index = () =>
+            result.ShouldNotBeNull();
+    }
+
+    public class when_calling_translations : with_a_home_controller
+    {
+        static ActionResult result;
+
+        Because of = () =>
+            result = controller.Translations();
+
+        It should_return_translations = () =>
+            result.ShouldNotBeNull();
+    }
+
+    public class when_calling_zen_garden :with_a_home_controller
+    {
+        static ActionResult result;
+
+        Because of = () =>
+            result = controller.ZenGarden();
+
+        It shoould_return_zengarden = () =>
+            result.ShouldNotBeNull();
+    }
 }
